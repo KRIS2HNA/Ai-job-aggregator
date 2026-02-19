b2c57e91560285347c56ab08b857a6a8c629dc1e
M K VAMSI <100195281+KRIS2HNA@users.noreply.github.com>
2026-02-19 14:27:34 +0530

    python

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from .database import engine, SessionLocal
-from .models import Base, Job
+from .models import Base, Job, Skill, JobSkill
+from sqlalchemy.exc import IntegrityError
 
 app = FastAPI()
 
@@ -30,3 +31,49 @@ def add_test_job():
     db.close()
     
     return {"message": "Test job inserted successfully"}
+
+@app.get("/add-job-with-skills")
+def add_job_with_skills():
+    db = SessionLocal()
+    
+    # Create a new job
+    try:
+        job = Job(
+            title = "AI Engineer",
+            company = "Google",
+            location = "Bangalore",
+            description = "AI/ML role",
+            apply_link = "https://google.com/careers/ai-engineer-1",
+            source = "Google Careers"
+        )
+    
+        db.add(job) 
+        db.commit()
+        db.refresh(job)
+    except IntegrityError:
+        db.rollback()
+        db.close()
+        return {"message": "Job with this apply_link already exists"}
+
+    
+    # step 2: create skills
+    skill_names = ["Python", "Machine Learning", "Deep Learning"]
+    
+    skills = []
+    for name in skill_names:
+        new_skill = Skill(name=name)
+        db.add(new_skill)
+        db.commit()
+        db.refresh(new_skill)
+        skills.append(new_skill)
+        
+    # step 3: link job and skills
+    
+    for s in skills:
+        link = JobSkill(job_id = job.id, skill_id= s.id)
+        db.add(link)
+        
+    db.commit()
+    db.close()
+    
+    return {"message": "job with skills added successfully"}
--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
-from sqlalchemy import Column, Integer, Text
+from sqlalchemy import Column, Integer, Text, DateTime, String, ForeignKey
+from sqlalchemy.sql import func
 from .database import Base
+from sqlalchemy.orm import relationship
+
 
 class Job(Base):
     __tablename__ = "jobs"
@@ -7,3 +10,22 @@ class Job(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(Text, nullable=False)
     company = Column(Text, nullable=False)
+    location = Column(String(255))
+    description  = Column(Text)
+    apply_link = Column(String(500), unique=True)
+    source = Column(String(100))
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    
+class Skill(Base):
+    __tablename__ = "skills"
+    
+    id = Column(Integer, primary_key=True)
+    
+    name = Column(String(100), unique=True, nullable=False)
+    
+class JobSkill(Base):
+    __tablename__ = "job_skills"
+    
+    job_id = Column(Integer, ForeignKey("jobs.id"), primary_key= True)
+    skills_id = Column(Integer, ForeignKey("skills.id"), primary_key= True)
+        
